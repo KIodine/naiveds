2aca3f2eb0ec1841d08f8df87f173b720f5f0ec8
frez <frez@example.com>
2019-12-18 21:56:06 +0800

    Replace `NULL` to specially forged `nil`
    Replace all `NULL` occurences in `rbtree_set` and `rbtree_delete` with `nil`.

--- rbtree/rbtree.c
+++ rbtree/rbtree.c
@@ -1,6 +1,14 @@
 #include "rbtree.h"
 
-static
+
+#ifdef __GNUC__
+    #define FORCE_INLINE __attribute__((always_inline))
+#elif
+    #define FORCE_INLINE
+#endif
+
+
+static inline FORCE_INLINE
 struct rbtnode *node_alloc(int key, int val){
     struct rbtnode *node = NULL;
     node = calloc(1, sizeof(struct rbtnode));
@@ -10,41 +18,59 @@ struct rbtnode *node_alloc(int key, int val){
 }
 
 static
-void tree_purge(struct rbtnode* start){
-    if (start == NULL){
+struct rbtnode *rbtnode_alloc(struct rbtree *tree, int key, int val){
+    struct rbtnode *node = NULL;
+    node = node_alloc(key, val);
+    /*
+     * the color of node is naturally red because of the
+     * order of enum order of COLOR_* constants.
+     */
+    node->parent = tree->nil;
+    node->left   = tree->nil;
+    node->right  = tree->nil;
+    return node;
+}
+
+static
+void tree_purge(struct rbtnode* start, struct rbtnode *nil){
+    if (start == nil){
         return;
     }
-    tree_purge(start->left);
-    tree_purge(start->right);
+    tree_purge(start->left, nil);
+    tree_purge(start->right, nil);
     free(start);
     return;
 }
 
 /* TODO modify these functions */
 static
-struct rbtnode **find_precedence(struct rbtnode **start){
+struct rbtnode **find_precedence(
+        struct rbtnode **start, struct rbtnode *nil
+    ){
     /* find the rightmost node of left subtree */
     struct rbtnode **can;
     can = &(*start)->left;
-    if (*can == NULL){
-        return NULL;
+    if (*can == nil){
+        return nil;
     }
-    for (;(*can)->right != NULL;){
+    for (;(*can)->right != nil;){
         can = &(*can)->right;
     }
     return can;
 }
 
 static
-struct rbtnode **find_successor(struct rbtnode **start){
+struct rbtnode **find_successor(
+        struct rbtnode **start, struct rbtnode *nil
+    ){
     /* find the leftmost node of right subtree */
     struct rbtnode **can;
     can = &(*start)->right;
-    if (*can == NULL){
-        return NULL;
+    if (*can == nil){
+        return nil;
     }
     /* only modify parent if candidate exists */
-    for (;(*can)->left != NULL;){
+    for (;(*can)->left != nil;){
         can = &(*can)->left;
     }
     return can;
@@ -65,33 +91,42 @@ void delete_fix(struct rbtnode *node);
 
 struct rbtree *rbtree_alloc(void){
     struct rbtree *tree = NULL;
+    struct rbtnode *nil = NULL;
     tree = calloc(1, sizeof(struct rbtree));
+    nil = node_alloc(0, 0);
+    nil->color  = COLOR_BLACK;
+    nil->parent = nil;
+    nil->left   = nil;
+    nil->right  = nil;
+    tree->nil = nil;
+    tree->root = nil;
     return tree;
 }
 
 void rbtree_purge(struct rbtree *tree){
-    tree_purge(tree->root);
+    tree_purge(tree->root, tree->nil);
     tree->count = 0;
     return;
 }
 
 void rbtree_free(struct rbtree *tree){
-    tree_purge(tree->root);
+    tree_purge(tree->root, tree->nil);
+    free(tree->nil);
     free(tree);
     return;
 }
 
 int rbtree_set(struct rbtree *tree, int key, int val){
-    struct rbtnode *new = NULL, **indirect;
+    struct rbtnode *new, *nil = tree->nil, **indirect;
     indirect = &tree->root;
     for (;;){
-        // this field is containing NULL
-        if ((*indirect) == NULL){
+        // this field is containing "NIL"
+        if ((*indirect) == nil){
             /*
                 we've found an empty place, implies no corresponding key
                 was found.
             */
-            new = node_alloc(key, val);
+            new = rbtnode_alloc(tree, key, val);
             *indirect = new;
             tree->count++;
             break;
@@ -112,9 +147,9 @@ int rbtree_set(struct rbtree *tree, int key, int val){
 }
 
 int rbtree_get(struct rbtree *tree, int key, int *res){
-    struct rbtnode *cur;
+    struct rbtnode *cur, *nil = tree->nil;
     cur = tree->root;
-    for (;cur != NULL;){
+    for (;cur != nil;){
         if (cur->key == key){
             *res = cur->val;
             break;
@@ -125,34 +160,36 @@ int rbtree_get(struct rbtree *tree, int key, int *res){
             cur = cur->right;
         }
     }
-    if (cur == NULL){
+    if (cur == nil){
         return RBT_NOELEM;
     }
     return RBT_OK;
 }
 
 int rbtree_delete(struct rbtree *tree, int key){
-    struct rbtnode **indirect = &tree->root, **victim, *next, *hold;
+    struct rbtnode
+        **indirect = &tree->root, **victim,
+        *next, *hold, *nil = tree->nil;
 
-    for (;*indirect != NULL;){
+    for (;*indirect != nil;){
         if ((*indirect)->key == key){
             /* do delete stuff */
-            victim = find_precedence(indirect);
+            victim = find_precedence(indirect, nil);
             // we can abuse pointer to make even shorter/cleaner code here.
-            if (victim != NULL){
+            if (victim != nil){
                 // abuse ver: *victim = (*victim)->left;
                 next = (*victim)->left;
                 break;
             }
-            victim = find_successor(indirect);
-            if (victim != NULL){
+            victim = find_successor(indirect, nil);
+            if (victim != nil){
                 next = (*victim)->right;
                 break;
             }
             /* it is a leaf node */
             free(*indirect);
             /* set the branch to NULL */
-            *indirect = NULL;
+            *indirect = nil;
             /* bypass the after loop condition check and victim-killing */
             goto decr_count;
         }
@@ -163,7 +200,7 @@ int rbtree_delete(struct rbtree *tree, int key){
         }
         /* then continue */
     }
-    if (*indirect == NULL){
+    if (*indirect == nil){
         /* can't find the entry */
         return RBT_NOELEM;
     }
