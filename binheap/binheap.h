c33adda701e278f7c3e8196a20085ce685653085
frez <frez@example.com>
2020-08-25 16:50:45 +0800

    Clean binheap for rewrite

--- binheap/binheap.h
+++ binheap/binheap.h
@@ -1,35 +1,6 @@
 #ifndef BINHEAP_H
 #define BINHEAP_H
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <assert.h>
-
-
-#define INIT_CAPACITY 32
-
-
-struct maxheap {
-    int count, capacity;
-    int *arr;
-};
-
-enum {
-    MAXHEAP_OK,
-    MAXHEAP_NOELEM,
-};
-
-
-void maxheapify(int *arr, unsigned int len);
-
-struct maxheap *maxheap_alloc(void);
-void maxheap_free(struct maxheap *heap);
-void maxheap_purge(struct maxheap *heap);
-
-int maxheap_insert(struct maxheap *heap, int val);
-int maxheap_peek(struct maxheap *heap, int *res);
-int maxheap_extract(struct maxheap *heap, int *res);
 
 
 #endif
